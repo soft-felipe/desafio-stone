645f04e462dc4942a15c61d23272c7183b98f289
soft-felipe <felipem791@gmail.com>
2024-11-04 22:14:47 -0300

    feat: add docker services

---
+++ src/main/java/com/felipemoreira/dockermanager/services/DockerService.java
@@ -0,0 +1,47 @@
+package com.felipemoreira.dockermanager.services;
+
+import com.github.dockerjava.api.DockerClient;
+import com.github.dockerjava.api.model.Container;
+import com.github.dockerjava.api.model.Image;
+import lombok.AllArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@AllArgsConstructor
+@Service
+public class DockerService {
+
+    private static final Logger logger = LoggerFactory.getLogger(DockerService.class);
+    private final DockerClient dockerClient;
+
+    public List<Container> listContainers(boolean all) {
+        return dockerClient.listContainersCmd().withShowAll(all).exec();
+    }
+
+    public List<Image> listImages() {
+        return dockerClient.listImagesCmd().exec();
+    }
+
+    public List<Image> filterImages(String imageName) {
+        return dockerClient.listImagesCmd().withImageNameFilter(imageName).exec();
+    }
+
+    public void startContainer(String containerId) {
+        dockerClient.startContainerCmd(containerId).exec();
+    }
+
+    public void stopContainer(String containerId) {
+        dockerClient.stopContainerCmd(containerId).exec();
+    }
+
+    public void deleteContainer(String containerId) {
+        dockerClient.removeContainerCmd(containerId).exec();
+    }
+
+    public void createContainer(String imageName) {
+        dockerClient.createContainerCmd(imageName).exec();
+    }
+}
